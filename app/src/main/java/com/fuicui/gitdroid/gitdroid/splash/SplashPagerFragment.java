95aa9757fbe1dbafc48cda77c8c22247c3a13465
yuanchao <yuanchao@feicuiedu.com>
2016-08-23 16:45:04 +0800

    ViewPager页面动画效果展示

--- app/src/main/java/com/fuicui/gitdroid/gitdroid/splash/SplashPagerFragment.java
+++ app/src/main/java/com/fuicui/gitdroid/gitdroid/splash/SplashPagerFragment.java
@@ -12,6 +12,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.fuicui.gitdroid.gitdroid.R;
+import com.fuicui.gitdroid.gitdroid.splash.pager.Pager2;
 
 import butterknife.BindColor;
 import butterknife.BindView;
@@ -51,15 +52,17 @@ public class SplashPagerFragment extends Fragment {
 
         //ViewPager的监听
         viewPager.addOnPageChangeListener(pageColorListener);
+        viewPager.addOnPageChangeListener(phoneViewListener);
 
     }
 
-    //设置的ViewPager颜色的监听
+    //设置的ViewPager背景颜色的监听
     private ViewPager.OnPageChangeListener pageColorListener = new ViewPager.OnPageChangeListener() {
 
         ArgbEvaluator argbEvaluator = new ArgbEvaluator();
 
         @Override public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            //
             if (position == 0) {
                 int color = (int) argbEvaluator.evaluate(positionOffset, colorGreen, colorRed);
                 frameLayout.setBackgroundColor(color);
@@ -74,6 +77,44 @@ public class SplashPagerFragment extends Fragment {
 
         @Override public void onPageSelected(int position) {
 
+        }
+
+        @Override public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    //实现“手机”的动画（缩放，移动，透明度）
+    private ViewPager.OnPageChangeListener phoneViewListener = new ViewPager.OnPageChangeListener() {
+
+        @Override public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            if (position==0){
+                //手机缩放的效果
+                /**
+                 * positionOffset  0-1
+                 */
+                float scale = 0.5f +positionOffset*0.7f;
+                layoutPhone.setScaleY(scale);
+                layoutPhone.setScaleX(scale);
+
+                //手机移动的效果
+                int scroll = (int) ((positionOffset-1)*360);
+                layoutPhone.setTranslationX(scroll);
+
+                //手机内字体图片透明度的改变
+                ivPhoneFont.setAlpha(positionOffset);
+            }
+            //手机从第二页面切换动画
+            if (position==1){
+                layoutPhone.setTranslationX(-positionOffsetPixels);
+            }
+        }
+
+        @Override public void onPageSelected(int position) {
+            if (position==2){
+                Pager2 pager2 = (Pager2) adapter.getView(position);
+                pager2.showAnimation();
+            }
         }
 
         @Override public void onPageScrollStateChanged(int state) {
